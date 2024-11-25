7b2eda18baa64ba877d214ff76d729784e859cb8
Victor B. <contact@itsvic.dev>
2024-11-25 19:58:31 +0100

    woooo

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use color_eyre::eyre::{Context, Result};
 use serenity::{
+    all::CreateAttachment,
     async_trait,
     builder::ExecuteWebhook,
     http::Http,
@@ -16,7 +17,7 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: serenity::prelude::Context, msg: Message) {
+    async fn message(&self, _ctx: serenity::prelude::Context, msg: Message) {
         debug!("got a message: {msg:?}");
     }
 }
@@ -33,6 +34,22 @@ pub async fn broadcast_message(message: &core::Message) -> Result<()> {
             "{} ({})",
             &message.author.display_name, &message.author.username
         ));
+
+    let mut attachments: Vec<CreateAttachment> = vec![];
+    for file in &message.attachments {
+        let attachment_old = CreateAttachment::path(&file.file.path).await?;
+        let attachment = if file.spoilered {
+            CreateAttachment::bytes(
+                attachment_old.data,
+                format!("SPOILER_{}", &file.file.name).to_owned(),
+            )
+        } else {
+            attachment_old
+        };
+        attachments.push(attachment);
+    }
+
+    let builder = builder.add_files(attachments);
 
     webhook.execute(&http, false, builder).await?;
 
