9c66c7478186b36636e97e456e218c2052199f5b
Victor B. <contact@itsvic.dev>
2024-11-26 16:43:39 +0100

    fuck telegram

--- src/telegram/broadcast.rs
+++ src/telegram/broadcast.rs
@@ -5,6 +5,7 @@ use crate::{
 use color_eyre::eyre::Result;
 use serenity::async_trait;
 use teloxide::{
+    payloads::SendMessageSetters,
     prelude::Requester,
     types::{ChatId, Recipient},
 };
@@ -14,13 +15,19 @@ use super::TelegramBridge;
 #[async_trait]
 impl BroadcastReceiver for TelegramBridge {
     async fn receive(&self, message: &core::Message) -> Result<()> {
-        let text = format!("**{}**\n{}", &message.author.full_name(), &message.content);
+        let text = format!("*{}*\n{}", &message.author.full_name(), &message.content)
+            .replace("(", "\\(")
+            .replace(")", "\\)")
+            .replace("[", "\\[")
+            .replace("]", "\\]")
+            .replace(".", "\\.");
 
         if message.attachments.len() > 0 {
             todo!();
         } else {
             self.bot
                 .send_message(Recipient::Id(ChatId(self.chat_id)), &text)
+                .parse_mode(teloxide::types::ParseMode::MarkdownV2)
                 .await?;
         }
 
