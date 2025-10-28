78697f241a519051c0c20673cf52a90dd139402d
Victor B. <contact@itsvic.dev>
2025-10-28 22:50:07 +0100

    working test???

--- nix/package.nix
+++ nix/package.nix
@@ -1,8 +1,8 @@
-{ rustPlatform }:
+{ lib, rustPlatform }:
 rustPlatform.buildRustPackage rec {
   pname = "oxibridge";
   version = "0.1.0";
-  src = ../.;
+  src = lib.sources.sourceFilesBySuffices [ ".nix" ] (lib.cleanSource ../.);
   cargoLock = { lockFile = ../Cargo.lock; };
 
   meta = { mainProgram = "oxibridge"; };
--- nix/test.nix
+++ nix/test.nix
@@ -6,16 +6,36 @@ pkgs.nixosTest {
     imports = [ self.nixosModules.oxibridge ];
     services.oxibridge = {
       enable = true;
-      configFile = pkgs.replaceVars ./config-test.yml { "src" = ./src.txt; };
+      configFile = pkgs.writeText "config.yml" ''
+        backends:
+          src:
+            # the "file" backend reads lines from a file and sends them as messages
+            # and writes lines to a file when receiving messages
+            kind: file
+            token: ${./src.txt}
+
+          dst:
+            kind: file
+            token: /var/lib/oxibridge/dst.txt
+
+        groups:
+          test:
+            src:
+              readonly: true
+            dst:
+              writeonly: true
+      '';
     };
 
-    systemd.services.oxibridge.unitConfig.RuntimeDirectory = "oxibridge";
+    systemd.services.oxibridge.serviceConfig.StateDirectory = "oxibridge";
   };
 
   testScript = ''
     import time
-    machine.wait_for_unit("oxibridge.service");
-    time.sleep(5)
-    # TODO: read dst.txt from service, compare against known good hash
+    machine.wait_for_unit("oxibridge.service")
+    time.sleep(5)  # wait for messages to be processed
+
+    # read dst.txt from service, expect known hash
+    machine.succeed("sha256sum /var/lib/oxibridge/dst.txt | grep 2edc4d35d0fcdb59b8b88a0e6140e01f207bea18a52a11c3a55d06c6d409aac2")
   '';
 }
