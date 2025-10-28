d17836f6ec6b0ce04094aeb7db0f4ad5d564e36a
Victor B. <contact@itsvic.dev>
2025-10-28 22:59:59 +0100

    filter out /nix in rust package

--- nix/package.nix
+++ nix/package.nix
@@ -2,7 +2,10 @@
 rustPlatform.buildRustPackage rec {
   pname = "oxibridge";
   version = "0.1.0";
-  src = lib.sources.sourceFilesBySuffices [ ".nix" ] (lib.cleanSource ../.);
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.difference ../. ./nix;
+  };
   cargoLock = { lockFile = ../Cargo.lock; };
 
   meta = { mainProgram = "oxibridge"; };
